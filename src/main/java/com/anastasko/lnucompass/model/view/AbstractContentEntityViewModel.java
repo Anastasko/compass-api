8083c8c648da0ac95728559c72d037170187d939
volodja <volodja.extrim@gmail.com>
2017-03-10 20:17:41 +0200

    big bug

--- src/main/java/com/anastasko/lnucompass/model/view/AbstractContentEntityViewModel.java
+++ src/main/java/com/anastasko/lnucompass/model/view/AbstractContentEntityViewModel.java
@@ -16,6 +16,7 @@ public class AbstractContentEntityViewModel extends AbstractEntityViewModel {
 	}
 
 	public AbstractContentEntityViewModel(AbstractContentEntity entity) {
+		super(entity);
 		setVersion(entity.getItem().getModified().getTime());
 	}
 
